9ea276c601f3f0d21bcbdec0a221afeb8015bda2
SmallPig1997 <664235822@qq.com>
2020-07-17 20:40:24 +0800

    新增受击管理器

---
+++ Assets/Scripts/BattleManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CapsuleCollider))]
+public class BattleManager : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        print("hit");
+    }
+    
+    
+}
