636711b441335fc6e05260e7670279bcbe58f0fa
SmallPig1997 <664235822@qq.com>
2020-07-18 21:17:54 +0800

    武器开关

---
+++ Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    public BoxCollider weaponCollider;
+    
+    public void WeaponEnable()
+    {
+        weaponCollider.enabled = true;
+    }
+
+    public void WeaponDisable()
+    {
+        weaponCollider.enabled = false;
+    }
+}
