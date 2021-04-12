2fc4c4af5cabd4168981f3a366acc3fe7d160304
SmallPig1997 <664235822@qq.com>
2021-04-12 12:24:41 +0800

    更换按钮

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -32,29 +32,29 @@ public class GameManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    private void OnGUI()
+    public void OnSword()
     {
-        if (GUI.Button(new Rect(10, 10, 150, 30), "R: Sword"))
-        {
-            weaponManager.UnloadWeapon("R");
-            weaponManager.UpdateWeaponCollider("R", weaponFactory.CreateWeapon("Sword", "R", weaponManager));
-            weaponManager.ChangeDualHands(false);
-        }
-        if (GUI.Button(new Rect(10, 50, 150, 30), "R: Falchion"))
-        {
-            weaponManager.UnloadWeapon("R");
-            weaponManager.UpdateWeaponCollider("R", weaponFactory.CreateWeapon("Falchion", "R", weaponManager));
-            weaponManager.ChangeDualHands(true);
-        }
-        if (GUI.Button(new Rect(10, 90, 150, 30), "R: Mace"))
-        {
-            weaponManager.UnloadWeapon("R");
-            weaponManager.UpdateWeaponCollider("R", weaponFactory.CreateWeapon("Mace", "R", weaponManager));
-            weaponManager.ChangeDualHands(false);
-        }
-        if (GUI.Button(new Rect(10, 130, 150, 30), "R: Clear All Weapon"))
-        {
-            weaponManager.UnloadWeapon("R");
-        }
+        weaponManager.UnloadWeapon("R");
+        weaponManager.UpdateWeaponCollider("R", weaponFactory.CreateWeapon("Sword", "R", weaponManager));
+        weaponManager.ChangeDualHands(false);
+    }
+
+    public void OnFalchion()
+    {
+        weaponManager.UnloadWeapon("R");
+        weaponManager.UpdateWeaponCollider("R", weaponFactory.CreateWeapon("Falchion", "R", weaponManager));
+        weaponManager.ChangeDualHands(true);
+    }
+
+    public void OnRace()
+    {
+        weaponManager.UnloadWeapon("R");
+        weaponManager.UpdateWeaponCollider("R", weaponFactory.CreateWeapon("Mace", "R", weaponManager));
+        weaponManager.ChangeDualHands(false);
+    }
+
+    public void OnClearAll()
+    {
+        weaponManager.UnloadWeapon("R");
     }
 }
